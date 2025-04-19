72794f07d862346e8c28d954cad3fae202984d75
Kfcaio <caio96@tuta.io>
2025-04-19 16:49:43 -0300

    fix: cors no ambiente de dev

--- src/api/src/server.ts
+++ src/api/src/server.ts
@@ -13,6 +13,21 @@ process.env.projectDir = backendPath;
 const app = express();
 const port = process.env.SERVER_PORT;
 
+// evita erro de cors em ambiente de desenvolvimento
+app.use((req, res, next) => {
+  console.log(`METHOD: ${req.method}, URL: ${req.originalUrl}, HOSTNAME: ${req.hostname}`);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    res.status(204);
+  }
+
+  next();
+});
+
 MiddlewareManagement.register(app);
 RouterManagement.register(app);
 
