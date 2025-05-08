63164b95fa2540f857cad1689adc95fe63e2e7e2
Iago Oliveira <iago.oli2000@gmail.com>
2025-05-08 19:49:42 -0300

    Update src/api/prisma/migrations/20250507210757_add_cascade_delete_to_payment_fee_relation/migration.sql
    Co-authored-by: Caio <caio.ignatz@jusbrasil.com.br>

--- src/api/prisma/migrations/20250507210757_add_cascade_delete_to_payment_fee_relation/migration.sql
+++ src/api/prisma/migrations/20250507210757_add_cascade_delete_to_payment_fee_relation/migration.sql
@@ -2,7 +2,7 @@
 ALTER TABLE `Payment` DROP FOREIGN KEY `Payment_feeId_fkey`;
 
 -- DropIndex
-DROP INDEX `Payment_feeId_fkey` ON `payment`;
+DROP INDEX `Payment_feeId_fkey` ON `Payment`;
 
 -- AddForeignKey
 ALTER TABLE `Payment` ADD CONSTRAINT `Payment_feeId_fkey` FOREIGN KEY (`feeId`) REFERENCES `Fee`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
